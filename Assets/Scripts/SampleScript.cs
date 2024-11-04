e3bbc2d73141ce00f931a4d10553451594e75a90
PJH6029 <top231902@naver.com>
2024-11-04 17:22:18 +0900

    Add pre commit hook for formatting c# code

---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BoxManager : MonoBehaviour {
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
