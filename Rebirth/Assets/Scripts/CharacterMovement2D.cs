5c3e3f6554a66ca06f30d60fbf705af675142856
boradorish <snusnowhite@snu.ac.kr>
2024-11-17 18:25:41 +0900

    2D Hero 애니메이션을 만든다.

--- Rebirth/Assets/Scripts/CharacterMovement2D.cs
+++ Rebirth/Assets/Scripts/CharacterMovement2D.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CharacterMovement2D : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Animator animator;
 
     private Rigidbody2D rb;
 
@@ -13,12 +14,45 @@ public class CharacterMovement2D : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal"); 
         movement.y = Input.GetAxisRaw("Vertical");  
+        animator.SetFloat("speed", movement.sqrMagnitude);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.SetBool("isBack", true);
+            animator.SetBool("isFront", false);
+            animator.SetBool("isLeft", false);
+            animator.SetBool("isRight", false);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.SetBool("isFront", true);
+            animator.SetBool("isBack", false); 
+            animator.SetBool("isLeft", false);
+            animator.SetBool("isRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            animator.SetBool("isLeft", true);
+            animator.SetBool("isBack", false); 
+            animator.SetBool("isFront", false);
+            animator.SetBool("isRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            animator.SetBool("isRight", true);
+            animator.SetBool("isBack", false); 
+            animator.SetBool("isFront", false);
+            animator.SetBool("isLeft", false);
+        }
     }
 
     void FixedUpdate()
