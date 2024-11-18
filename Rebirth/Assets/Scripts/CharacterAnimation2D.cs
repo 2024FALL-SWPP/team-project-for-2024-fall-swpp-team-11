1a5779ac91b7c90b41db82819b46e71dfac0d579
PJH6029 <top231902@naver.com>
2024-11-18 18:00:04 +0900

    refactor: 움직이지 않을 때의 애니메이션 수정
    - Movement가 없을 때 stay state로 transition

--- Rebirth/Assets/Scripts/CharacterAnimation2D.cs
+++ Rebirth/Assets/Scripts/CharacterAnimation2D.cs
@@ -15,29 +15,39 @@ public class CharacterAnimation2D : MonoBehaviour
     {
         if (!animator) return;
 
-        animator.SetBool("isBack", false);
-        animator.SetBool("isFront", false);
-        animator.SetBool("isLeft", false);
-        animator.SetBool("isRight", false);
-
+        // speed
         animator.SetFloat("speed", movement.sqrMagnitude);
 
-        if (movement.y > 0) 
+        // direction
+        if (movement.sqrMagnitude == 0) return; // no movement. keep the last direction
+
+        ResetAnimationParameters();
+
+        if (movement.y > 0)
         {
             animator.SetBool("isBack", true);
         }
-        else if (movement.y < 0) 
+        else if (movement.y < 0)
         {
             animator.SetBool("isFront", true);
         }
-        else if (movement.x < 0) 
+        else if (movement.x < 0)
         {
             animator.SetBool("isLeft", true);
         }
-        else if (movement.x > 0) 
+        else if (movement.x > 0)
         {
             animator.SetBool("isRight", true);
         }
     }
 
+    private void ResetAnimationParameters()
+    {
+        if (!animator) return;
+
+        animator.SetBool("isBack", false);
+        animator.SetBool("isFront", false);
+        animator.SetBool("isLeft", false);
+        animator.SetBool("isRight", false);
+    }
 }
