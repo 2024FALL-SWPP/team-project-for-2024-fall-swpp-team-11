6e03cf0d5cf2a0d4a5bdbaf138d430a6efcddb60
김시현 <dbsejrdn@snu.ac.kr>
2024-11-29 19:23:59 +0900

    feat: Add picking sound, quest sound, dungeon BGM Related task-id: 66

--- Rebirth/Assets/Scripts/Inventory/Item/WorldItem/WorldItem.cs
+++ Rebirth/Assets/Scripts/Inventory/Item/WorldItem/WorldItem.cs
@@ -4,10 +4,17 @@ public class WorldItem : MonoBehaviour, IInteractable
 {
     public ItemData itemData;
     private Outline outline;
+    public AudioClip PickingSound;
+    private AudioSource audioSource;
 
     private void Awake()
     {
         outline = GetComponent<Outline>();
+
+        // AudioSource 초기화
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false; // 자동 재생 방지
+        audioSource.clip = PickingSound; // AudioClip 설정
     }
 
     public void Interact()
@@ -17,7 +24,14 @@ public class WorldItem : MonoBehaviour, IInteractable
             InventoryManager.Instance.AddItem(itemData);
         }
 
-        Destroy(gameObject);
+        // 아이템 획득 소리 재생
+        if (audioSource && PickingSound)
+        {
+            audioSource.Play();
+        }
+
+        // 오브젝트 파괴 지연 (소리 재생 후 파괴되도록)
+        Destroy(gameObject, 0.3f);
     }
 
     public void OnFocus()
