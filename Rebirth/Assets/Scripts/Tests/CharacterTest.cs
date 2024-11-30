5455d38f6a70002b33a30d82c1ac889d4b4484e1
PJH6029 <top231902@naver.com>
2024-12-01 01:20:33 +0900

    test: Mouse and keyboard does not work

--- Rebirth/Assets/Scripts/Tests/CharacterTest.cs
+++ Rebirth/Assets/Scripts/Tests/CharacterTest.cs
@@ -5,50 +5,126 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
 using UnityEngine.InputSystem.Utilities;
+using UnityEngine.InputSystem.LowLevel;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 public class CharacterTest : InputTestFixture
 {
     private static readonly string logPrefix = "[CharacterTest] ";
     private static readonly string heroPrefabPath = "Assets/Prefabs/GlobalPrefabs/Character/3D Hero.prefab";
-    private GameObject heroPrefab;
+    private static readonly string cameraPrefabPath = "Assets/Prefabs/GlobalPrefabs/Camera/MainCamera3D.prefab";
+    private static readonly string testSceneName = "Village3D";
+
+    private GameObject hero;
+
+    private void MoveMouse(Vector2 position)
+    {
+        InputSystem.QueueStateEvent(Mouse.current, new MouseState
+        {
+            position = position
+        });
+        InputSystem.Update();
+    }
+
+    private IEnumerator MoveMouseTime(Vector2 position, float time)
+    {
+        // move mout to position linearly in time
+        Vector2 start = Mouse.current.position.ReadValue();
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            MoveMouse(Vector2.Lerp(start, position, elapsedTime / time));
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveMouseTimeDelta(Vector2 position, float time)
+    {
+        // move mout to position linearly in time
+        Vector2 start = Mouse.current.position.ReadValue();
+        float elapsedTime = 0;
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            MoveMouse(start + (position - start) * elapsedTime / time);
+            yield return null;
+        }
+    }
+
+    private void ResetMouse()
+    {
+        Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+        InputSystem.QueueStateEvent(Mouse.current, new MouseState
+        {
+            position = screenCenter
+        });
+        InputSystem.Update();
+    }
+
 
     [UnitySetUp]
     public IEnumerator UnitySetup()
     {
-        AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(heroPrefabPath);
-        yield return handle;
-        Assert.IsTrue(handle.Status == AsyncOperationStatus.Succeeded, "Failed to load hero prefab");
+        ResetMouse();
 
-        heroPrefab = Object.Instantiate(handle.Result);
+        var loadOp = SceneManager.LoadSceneAsync(testSceneName, LoadSceneMode.Single);
+        while (!loadOp.isDone)
+        {
+            yield return null;
+        }
+
+        // AsyncOperationHandle<GameObject> handle = Addressables.LoadAssetAsync<GameObject>(heroPrefabPath);
+        // yield return handle;
+        // Assert.IsTrue(handle.Status == AsyncOperationStatus.Succeeded, "Failed to load hero prefab");
+        // heroPrefab = Object.Instantiate(handle.Result);
+
+        // handle = Addressables.LoadAssetAsync<GameObject>(cameraPrefabPath);
+        // yield return handle;
+        // Assert.IsTrue(handle.Status == AsyncOperationStatus.Succeeded, "Failed to load camera prefab");
+        // GameObject cameraPrefab = Object.Instantiate(handle.Result);
+
+        // var camera = cameraPrefab.GetComponent<CameraController3D>();
+        // Assert.IsNotNull(camera, "CameraController3D component not found");
+
+        // camera.enabled = true;
+        // camera.target = heroPrefab.transform;
+
+        hero = GameObject.FindWithTag("Player");
+
         Debug.Log(logPrefix + "Setup complete");
     }
 
     [UnityTearDown]
     public void UnityTeardown()
     {
-        if (heroPrefab != null)
-        {
-            Object.Destroy(heroPrefab);
-        }
+        // if (heroPrefab != null)
+        // {
+        //     Object.Destroy(heroPrefab);
+        // }
+        SceneManager.LoadScene("MainMenu");
     }
 
     [UnityTest]
     public IEnumerator TestPlayerMovement()
     {
         var keyboard = InputSystem.AddDevice<Keyboard>();
-        
-        Character3D character = heroPrefab.GetComponent<Character3D>();
-        Assert.IsNotNull(character, "Character3D component not found");
+        var mouse = InputSystem.AddDevice<Mouse>();
 
-        character.enabled = true;
-        
-        Press(keyboard.upArrowKey);
-        yield return new WaitForSeconds(0.1f);
+        Vector3 oldPosition = hero.transform.position;
+        Debug.Log(logPrefix + "Old position: " + oldPosition);
 
-        Assert.Greater(character.transform.position.z, 0, "Character did not move forward");
+        // Press(keyboard.upArrowKey);
+        // yield return new WaitForSeconds(2f);
+
+        yield return MoveMouseTimeDelta(new Vector2(100, 100), 2f);
+
+        Debug.Log(logPrefix + "New position: " + hero.transform.position);
+
+        Assert.Greater(hero.transform.position.z, oldPosition.z, "Character did not move forward");
 
         Release(keyboard.upArrowKey);
     }
