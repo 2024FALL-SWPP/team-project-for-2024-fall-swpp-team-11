dbe5cf5572f79391708246f3c097bbd152d684d3
boradorish <snusnowhite@snu.ac.kr>
2024-12-18 03:21:44 +0900

    fix/everything-3 - 이상한 묘약을 사용한 뒤 dialogue 추가

--- Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
+++ Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
@@ -7,10 +7,16 @@ public class WeirdPotion : UsableItem
     public string targetSceneName = "HeroHouse2D"; // 삭제할 오브젝트가 있는 씬 이름
     public string targetObjectName = "2DDungeonKey"; // 삭제할 오브젝트의 이름
 
+    public NPC sceneTransitionUsageNPC;
+
     public override void Use()
     {
         // 캐릭터 상태 변경
         CharacterStatusManager.Instance.SetPlayerState(PlayerState.IsToxified); // World Item will be invisible when state is IsToxified
         InventoryManager.Instance.RemoveItemByName("2DDungeonKey");
+
+        // Scene Transition을 섦명
+        NPC newNPC = Instantiate(sceneTransitionUsageNPC) as NPC;
+        newNPC.Interact();
     }
 }
