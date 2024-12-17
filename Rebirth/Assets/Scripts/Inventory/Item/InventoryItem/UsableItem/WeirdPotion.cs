b1f1b9ad3c561826d7671f1bab6d7f9ea0eddba5
boradorish <snusnowhite@snu.ac.kr>
2024-12-17 14:17:07 +0900

    fix/everything-2 -이상한 묘약을 사용한 후 시점 변환 방법 설명 dialogue를 띄움.

--- Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
+++ Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
@@ -7,10 +7,30 @@ public class WeirdPotion : UsableItem
     public string targetSceneName = "HeroHouse2D"; // 삭제할 오브젝트가 있는 씬 이름
     public string targetObjectName = "2DDungeonKey"; // 삭제할 오브젝트의 이름
 
+    public NPC SceneTransitionUsageNPC; // 사용 이후 띄울 dialogue를 가진 NPC
+    private bool hasInteractedWithSceneTransitionUsage = false; // 최초로 한 번만 실행.
+
     public override void Use()
     {
         // 캐릭터 상태 변경
         CharacterStatusManager.Instance.SetPlayerState(PlayerState.IsToxified); // World Item will be invisible when state is IsToxified
         InventoryManager.Instance.RemoveItemByName("2DDungeonKey");
+
+        if (!hasInteractedWithSceneTransitionUsage) 
+            {
+                hasInteractedWithSceneTransitionUsage = true;
+
+                NPC newNPC = Instantiate(SceneTransitionUsageNPC) as NPC;
+                if (newNPC != null)
+                {
+                    newNPC.Interact();
+                    return;
+                }
+                else
+                {
+                    Debug.LogError("NPC 프리팹이 제대로 할당되지 않았습니다.");
+                    return;
+                }
+            } 
     }
 }
