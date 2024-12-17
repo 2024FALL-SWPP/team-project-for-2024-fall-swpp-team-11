cf062f190f1a18836b49569f696e26fd5ae282ca
boradorish <snusnowhite@snu.ac.kr>
2024-12-17 14:39:16 +0900

    fix/everything-2-local -묘약 / 묘약의 해독제 사용 이후 dialogue 나타나게 함.

--- Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
+++ Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotion.cs
@@ -8,29 +8,21 @@ public class WeirdPotion : UsableItem
     public string targetObjectName = "2DDungeonKey"; // 삭제할 오브젝트의 이름
 
     public NPC SceneTransitionUsageNPC; // 사용 이후 띄울 dialogue를 가진 NPC
-    private bool hasInteractedWithSceneTransitionUsage = false; // 최초로 한 번만 실행.
 
     public override void Use()
     {
+        NPC newNPC = Instantiate(SceneTransitionUsageNPC) as NPC;
+        if (newNPC != null)
+        {
+            newNPC.Interact();
+        }
+        else
+        {
+            Debug.LogError("NPC 프리팹이 제대로 할당되지 않았습니다.");
+        }
+
         // 캐릭터 상태 변경
         CharacterStatusManager.Instance.SetPlayerState(PlayerState.IsToxified); // World Item will be invisible when state is IsToxified
         InventoryManager.Instance.RemoveItemByName("2DDungeonKey");
-
-        if (!hasInteractedWithSceneTransitionUsage) 
-            {
-                hasInteractedWithSceneTransitionUsage = true;
-
-                NPC newNPC = Instantiate(SceneTransitionUsageNPC) as NPC;
-                if (newNPC != null)
-                {
-                    newNPC.Interact();
-                    return;
-                }
-                else
-                {
-                    Debug.LogError("NPC 프리팹이 제대로 할당되지 않았습니다.");
-                    return;
-                }
-            } 
     }
 }
--- Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotionCure.cs
+++ Rebirth/Assets/Scripts/Inventory/Item/InventoryItem/UsableItem/WeirdPotionCure.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WeirdPotionCure", menuName = "Item/Weird Potion Cure")]
 public class WeirdPotionCure : UsableItem
 {
+    public NPC SceneTransitionUsageExitNPC; // 사용 이후 띄울 dialogue를 가진 NPC
+
     public override async void Use()
     {
         CharacterStatusManager.Instance.SetPlayerState(PlayerState.IsDetoxified);
@@ -10,6 +12,17 @@ public class WeirdPotionCure : UsableItem
         if (DimensionManager.Instance.GetCurrentDimension() == Dimension.THREE_DIMENSION)
         {
             await DimensionManager.Instance.SwitchDimension();
+        }
+
+        NPC newNPC = Instantiate(SceneTransitionUsageExitNPC) as NPC;
+        if (newNPC != null)
+        {
+            newNPC.Interact();
+        }
+        else
+        {
+            Debug.LogError("NPC 프리팹이 제대로 할당되지 않았습니다.");
+            return;
         }
     }
 }
